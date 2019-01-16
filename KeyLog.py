40ef45fd3df1eb53ee316246e93b9e6b328b9ab0
Rafał Daniś <r.danis88@gmail.com>
2019-01-16 21:26:06 +0100

    delete comment

--- KeyLog.py
+++ KeyLog.py
@@ -23,19 +23,6 @@ class KeyLogger:
 
         for key, programName in patternProgramName:
             if(self.interestingProgram(programName, event.WindowName)):
-                #print('MessageName:', event.MessageName)
-                #print('Message:', event.Message)
-                #print('Time:', event.Time)
-                #print('Window:', event.Window)
-                #print('WindowName:', event.WindowName.upper())
-                #print('Ascii:', event.Ascii, chr(event.Ascii))
                 print('Odnotowano Key:', event.Key)
-                #print('KeyID:', event.KeyID)
-                #print('ScanCode:', event.ScanCode)
-                #print('Extended:', event.Extended)
-                #print('Injected:', event.Injected)
-                #print('Alt', event.Alt)
-                #print('Transition', event.Transition)
-                #print('---')
 
         return True
